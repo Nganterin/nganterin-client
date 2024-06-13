738de8fdd52391042b83c4ba4598ecc358b8a2dd
Rama Diaz <ramadiaz221@gmail.com>
2024-06-13 17:47:40 +0700

    UPDATE of Search engine

--- src/app/search-result/hotel/page.jsx
+++ src/app/search-result/hotel/page.jsx
@@ -1,7 +1,14 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { Card, CardBody, Button, Input, Image } from "@nextui-org/react";
+import {
+  Card,
+  CardBody,
+  Button,
+  Input,
+  Image,
+  Skeleton,
+} from "@nextui-org/react";
 import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import Loading from "@/app/loading";
@@ -20,11 +27,11 @@ const Page = () => {
   const router = useRouter();
 
   const [isLoading, setIsLoading] = useState(true);
+  const [isFetching, setIsFetching] = useState(false);
   const [search, setSearch] = useState(searchValue || "");
   const [result, setResult] = useState([]);
 
   const fetchData = async () => {
-    setIsLoading(true);
     try {
       const response = await fetch(`${BASE_API}/hotels?search=${searchValue}`, {
         method: "GET",
@@ -49,11 +56,78 @@ const Page = () => {
     fetchData();
   }, [searchValue]);
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
+    setIsFetching(true);
+
     e.preventDefault();
-    router.push(
+    window.history.replaceState(
+      null,
+      null,
       `/search-result/hotel?search=${search}&dateStart=${dateStart}&dateEnd=${dateEnd}`
     );
+    try {
+      const response = await fetch(`${BASE_API}/hotels?search=${search}`, {
+        method: "GET",
+        headers: {
+          "X-Authorization": API_KEY,
+          Authorization: `Bearer ${user_token}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setResult(data.data);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  const FetchingSkeleton = () => {
+    for (var i = 0; i < 4; i++) {
+      return (
+        <div
+          className="shadow shadow-black/30 hover:shadow-lg hover:shadow-black/30 transition-all duration-500 rounded-2xl"
+          key={i}
+        >
+          <Card isBlurred className="border-none bg-background/50" shadow="sm">
+            <CardBody>
+              <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
+                <div className="relative col-span-6 md:col-span-4">
+                  <Skeleton className="rounded-lg">
+                    <div className="w-64 h-36 rounded-lg bg-default-300"></div>
+                  </Skeleton>
+                </div>
+
+                <div className="flex flex-col col-span-6 md:col-span-8">
+                  <div className="flex justify-between items-start">
+                    <div className="flex flex-col gap-2 w-full">
+                      <Skeleton className="w-36 rounded-lg">
+                        <div className="h-6 rounded-lg bg-default-300"></div>
+                      </Skeleton>
+                      <Skeleton className="w-36 rounded-lg">
+                        <div className="h-6 rounded-lg bg-default-300"></div>
+                      </Skeleton>
+                      <Skeleton className="rounded-lg">
+                        <div className="h-8 rounded-lg bg-default-300"></div>
+                      </Skeleton>
+                    </div>
+                    <Button
+                      isIconOnly
+                      className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
+                      radius="full"
+                      variant="light"
+                    ></Button>
+                  </div>
+                </div>
+              </div>
+            </CardBody>
+          </Card>
+        </div>
+      );
+    }
   };
 
   return (
@@ -71,71 +145,79 @@ const Page = () => {
                   value={search}
                   onChange={(e) => setSearch(e.target.value)}
                 />
-                <Button className="bg-sky-700" isIconOnly type="submit">
+                <Button
+                  className="bg-sky-700"
+                  isIconOnly
+                  type="submit"
+                  isLoading={isFetching}
+                >
                   <MagnifyingGlass size={24} color="#fff" />
                 </Button>
               </form>
             </div>
             {result.length != 0 && (
               <div className="grid grid-cols-2 gap-4 justify-center items-center">
-                {result.map((item, index) => {
-                  const image_array = item.hotel_photos;
-                  const imageUrl = JSON.parse(image_array);
-                  const price = parseInt(item.overnight_prices);
+                {isFetching && <FetchingSkeleton />}
 
-                  return (
-                    <div
-                      className="shadow shadow-black/30 hover:shadow-lg hover:shadow-black/30 transition-all duration-500 rounded-2xl"
-                      key={index}
-                    >
-                      <Card
-                        as={Link}
-                        href={`/detail/hotel/${item.id}`}
-                        isBlurred
-                        className="border-none bg-background/50"
-                        shadow="sm"
+                {!isFetching &&
+                  result.map((item, index) => {
+                    const image_array = item.hotel_photos;
+                    const imageUrl = JSON.parse(image_array);
+                    const price = parseInt(item.overnight_prices);
+
+                    return (
+                      <div
+                        className="shadow shadow-black/30 hover:shadow-lg hover:shadow-black/30 transition-all duration-500 rounded-2xl"
+                        key={index}
                       >
-                        <CardBody>
-                          <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-                            <div className="relative col-span-6 md:col-span-4">
-                              <Image
-                                alt="Hotel image"
-                                className="object-cover w-64 h-36"
-                                height={200}
-                                shadow="md"
-                                src={imageUrl[0]}
-                                width={200}
-                              />
-                            </div>
+                        <Card
+                          as={Link}
+                          href={`/detail/hotel/${item.id}`}
+                          isBlurred
+                          className="border-none bg-background/50"
+                          shadow="sm"
+                        >
+                          <CardBody>
+                            <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
+                              <div className="relative col-span-6 md:col-span-4">
+                                <Image
+                                  alt="Hotel image"
+                                  className="object-cover w-64 h-36"
+                                  height={200}
+                                  shadow="md"
+                                  src={imageUrl[0]}
+                                  width={200}
+                                />
+                              </div>
 
-                            <div className="flex flex-col col-span-6 md:col-span-8">
-                              <div className="flex justify-between items-start">
-                                <div className="flex flex-col gap-0 w-full">
-                                  <h3 className="font-semibold text-foreground/90">
-                                    {item.name}
-                                  </h3>
-                                  <p className="text-small text-foreground/80">
-                                    {item.city}, {item.state}
-                                  </p>
-                                  <h1 className="text-large text-right font-medium mt-2">
-                                    <span className="text-sm mr-2">from</span>
-                                    <span>Rp. {price.toLocaleString()}</span>
-                                  </h1>
+                              <div className="flex flex-col col-span-6 md:col-span-8">
+                                <div className="flex justify-between items-start">
+                                  <div className="flex flex-col gap-0 w-full">
+                                    <h3 className="font-semibold text-foreground/90">
+                                      {item.name}
+                                    </h3>
+                                    <p className="text-small text-foreground/80">
+                                      {item.city}, {item.state}
+                                    </p>
+                                    <h1 className="text-large text-right font-medium mt-2">
+                                      <span className="text-sm mr-2">from</span>
+                                      <span>Rp. {price.toLocaleString()}</span>
+                                    </h1>
+                                  </div>
+                                  <Button
+                                    isIconOnly
+                                    className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
+                                    radius="full"
+                                    variant="light"
+                                  ></Button>
                                 </div>
-                                <Button
-                                  isIconOnly
-                                  className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
-                                  radius="full"
-                                  variant="light"
-                                ></Button>
                               </div>
                             </div>
-                          </div>
-                        </CardBody>
-                      </Card>
-                    </div>
-                  );
-                })}
+                          </CardBody>
+                        </Card>
+                      </div>
+                    );
+                  })}
               </div>
             )}
           </div>
